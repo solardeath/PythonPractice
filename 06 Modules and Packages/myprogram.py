70c7b770c169c0a964d22a2aa0067b1e2a8154b3
Pronnoy <pronnoy1998@gmail.com>
2020-08-12 18:55:38 +0530

    How to create packages and files with folder structure

---
+++ 06 Modules and Packages/myprogram.py
@@ -0,0 +1,11 @@
+#import from Package
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import mysubscript
+
+some_main_script.report_main()
+mysubscript.sub_report()
+
+
+#import from module
+from mymodule import my_func
+my_func()
